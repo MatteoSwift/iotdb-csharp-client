72f3eca36b87786615d355566886ca2dfa0fd2c7
Matteo <matteo.swift@hotmail.com>
2022-04-28 14:09:20 +0800

    https://github.com/eedalong/Apache-IoTDB-Client-CSharp/tree/0.13.0.1

---
+++ Salvini.IoTDB.Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Salvini.IoTDB;
+
+namespace Salvini.IoTDB.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("test");
+            var session = new Session(password: "admin#123", host: "192.168.0.11");
+            await session.OpenAsync();
+            using var ds = await session.ExecuteQueryStatementAsync("select 2MW from root.lz limit 10");
+            var matrix = ds.ReadAsMatrix();
+
+            for (var i = 0; i < matrix.Length / matrix.Rank; i++)
+            {
+                for (var j = 0; j < matrix.Rank; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Salvini.IoTDB/ClientPool.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Concurrent;
+
+namespace Salvini.IoTDB;
+
+public class ClientPool:IEnumerable<Client>
+{
+    private ConcurrentQueue<Client> _clients { get; } = new ConcurrentQueue<Client>();
+ 
+    public void Add(Client client)
+    {
+        Monitor.Enter(_clients);
+        _clients.Enqueue(client);
+        Monitor.Pulse(_clients);
+        Monitor.Exit(_clients);
+    } 
+
+    public Client Take()
+    {
+        Monitor.Enter(_clients);
+        if (_clients.IsEmpty)
+        {
+            Monitor.Wait(_clients);
+        }
+        _clients.TryDequeue(out var client);
+        Monitor.Exit(_clients);
+        return client;
+    }
+
+    public void Clear()
+    {
+        foreach (var client in _clients)
+        {
+            try
+            {
+                client?.Transport?.Close();
+            }
+            catch (System.Exception e)
+            {
+
+            }
+        }
+        _clients.Clear();
+    }
+
+    public IEnumerator<Client> GetEnumerator()
+    {
+        return _clients.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _clients.GetEnumerator();
+    }
+}
